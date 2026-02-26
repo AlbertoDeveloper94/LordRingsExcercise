ace3735c4ccbf699986c39d12d24e6061291d23d
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:44:07 +0100

    feat: I add a Human class with the attributes, constructor and getters and setters. Also, I use a method to print the information and apply herence

--- src/main/java/org/example/alignment/good/Human.java
+++ src/main/java/org/example/alignment/good/Human.java
@@ -1,4 +1,49 @@
 package org.example.alignment.good;
 
-public class Human {
+import org.example.character.Character;
+
+public class Human extends Character {
+
+    private int versality;
+    private boolean deadlyCourage;
+    private boolean heroicAmbition;
+
+    public Human(String name, String weapon, String job, int age, int life, float speed, float size, int versality, boolean deadlyCourage, boolean heroicAmbition) {
+        super(name, weapon, job, age, life, speed, size);
+        this.versality = versality;
+        this.deadlyCourage = deadlyCourage;
+        this.heroicAmbition = heroicAmbition;
+    }
+
+    public int getVersality() {
+        return versality;
+    }
+
+    public void setVersality(int versality) {
+        this.versality = versality;
+    }
+
+    public boolean isDeadlyCourage() {
+        return deadlyCourage;
+    }
+
+    public void setDeadlyCourage(boolean deadlyCourage) {
+        this.deadlyCourage = deadlyCourage;
+    }
+
+    public boolean isHeroicAmbition() {
+        return heroicAmbition;
+    }
+
+    public void setHeroicAmbition(boolean heroicAmbition) {
+        this.heroicAmbition = heroicAmbition;
+    }
+
+    @Override
+    public String showInfoCharacter() {
+        return super.showInfoCharacter() +
+                "Versatilidad: " + versality
+                + "Coraje mortal: " + deadlyCourage
+                + "Ambición heroica: " + heroicAmbition;
+    }
 }
