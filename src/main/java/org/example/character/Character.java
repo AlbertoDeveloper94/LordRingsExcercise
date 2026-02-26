39d0b88fcc9b127731c83a808bee076c129cc694
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:03:38 +0100

    feat: I add attributes to character superclass

--- src/main/java/org/example/character/Character.java
+++ src/main/java/org/example/character/Character.java
@@ -1,4 +1,16 @@
 package org.example.character;
 
 public class Character {
+
+    private String name;
+    private String weapon;
+    private String job;
+
+    private int age;
+    private int life;
+
+    private float speed;
+    private float size;
+
+
 }
