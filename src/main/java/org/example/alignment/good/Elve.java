2a6cd2ca43c63bed682b73016504d1f0d5c0293a
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:35:10 +0100

    feat: I add a Elve class with the attributes, constructor and getters and setters. Also I use a method to print the information and herence

--- src/main/java/org/example/alignment/good/Elve.java
+++ src/main/java/org/example/alignment/good/Elve.java
@@ -1,4 +1,49 @@
 package org.example.alignment.good;
 
-public class Elve {
+import org.example.character.Character;
+
+public class Elve extends Character {
+
+    private int inmortality;
+    private boolean natureConnection;
+    private boolean ancestralMemory;
+
+    public Elve(String name, String weapon, String job, int age, int life, float speed, float size, int inmortality, boolean natureConnection, boolean ancestralMemory) {
+        super(name, weapon, job, age, life, speed, size);
+        this.inmortality = inmortality;
+        this.natureConnection = natureConnection;
+        this.ancestralMemory = ancestralMemory;
+    }
+
+    public int getInmortality() {
+        return inmortality;
+    }
+
+    public void setInmortality(int inmortality) {
+        this.inmortality = inmortality;
+    }
+
+    public boolean isNatureConnection() {
+        return natureConnection;
+    }
+
+    public void setNatureConnection(boolean natureConnection) {
+        this.natureConnection = natureConnection;
+    }
+
+    public boolean isAncestralMemory() {
+        return ancestralMemory;
+    }
+
+    public void setAncestralMemory(boolean ancestralMemory) {
+        this.ancestralMemory = ancestralMemory;
+    }
+
+    @Override
+    public String showInfoCharacter() {
+        return super.showInfoCharacter() +
+                "Inmortalidad: " + inmortality
+                + "Conexión con la naturaleza: " + natureConnection
+                + "Memoria ancestral: " + ancestralMemory;
+    }
 }
