780ae59f77daedbe2391d3defb5c4d28948a43b1
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:05:00 +0100

    feat: I add the constructor

--- src/main/java/org/example/character/Character.java
+++ src/main/java/org/example/character/Character.java
@@ -12,5 +12,13 @@ public class Character {
     private float speed;
     private float size;
 
-
+    public Character(String name, String weapon, String job, int age, int life, float speed, float size) {
+        this.name = name;
+        this.weapon = weapon;
+        this.job = job;
+        this.age = age;
+        this.life = life;
+        this.speed = speed;
+        this.size = size;
+    }
 }
