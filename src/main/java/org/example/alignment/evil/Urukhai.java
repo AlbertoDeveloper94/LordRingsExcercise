6a6b945c98dfef2e0bbd29f5892556e237e0b998
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:49:33 +0100

    feat: I add a Urukhai class with the attributes, constructor and getters and setters. Also, I use a method to print the information and apply herence

--- src/main/java/org/example/alignment/evil/Urukhai.java
+++ src/main/java/org/example/alignment/evil/Urukhai.java
@@ -1,4 +1,38 @@
 package org.example.alignment.evil;
 
-public class Urukhai {
+import org.example.character.Character;
+
+public class Urukhai extends Character {
+
+    private int highSunResistance;
+    private int magicResistance;
+
+    public Urukhai(String name, String weapon, String job, int age, int life, float speed, float size, int highSunResistance, int magicResistance) {
+        super(name, weapon, job, age, life, speed, size);
+        this.highSunResistance = highSunResistance;
+        this.magicResistance = magicResistance;
+    }
+
+    public int getMagicResistance() {
+        return magicResistance;
+    }
+
+    public void setMagicResistance(int magicResistance) {
+        this.magicResistance = magicResistance;
+    }
+
+    public int getHighSunResistance() {
+        return highSunResistance;
+    }
+
+    public void setHighSunResistance(int highSunResistance) {
+        this.highSunResistance = highSunResistance;
+    }
+
+    @Override
+    public String showInfoCharacter() {
+        return super.showInfoCharacter() +
+                "Resistencia alta al sol: " + highSunResistance
+                + "Resistencia a la magia: " + magicResistance;
+    }
 }
