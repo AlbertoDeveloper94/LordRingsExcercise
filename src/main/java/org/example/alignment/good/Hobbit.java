dfa1c2000ff414a0d3dd3ebeaf8b5e9e3a8e8e2c
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:39:20 +0100

    feat: I add a Hobbit class with the attributes, constructor and getters and setters. Also, I use a method to print the information and apply herence

--- src/main/java/org/example/alignment/good/Hobbit.java
+++ src/main/java/org/example/alignment/good/Hobbit.java
@@ -1,4 +1,49 @@
 package org.example.alignment.good;
 
-public class Hobbit {
+import org.example.character.Character;
+
+public class Hobbit extends Character {
+
+    private int stealth;
+    private boolean discretion;
+    private boolean luck;
+
+    public Hobbit(String name, String weapon, String job, int age, int life, float speed, float size, int stealth, boolean discretion, boolean luck) {
+        super(name, weapon, job, age, life, speed, size);
+        this.stealth = stealth;
+        this.discretion = discretion;
+        this.luck = luck;
+    }
+
+    public int getStealth() {
+        return stealth;
+    }
+
+    public void setStealth(int stealth) {
+        this.stealth = stealth;
+    }
+
+    public boolean isDiscretion() {
+        return discretion;
+    }
+
+    public void setDiscretion(boolean discretion) {
+        this.discretion = discretion;
+    }
+
+    public boolean isLuck() {
+        return luck;
+    }
+
+    public void setLuck(boolean luck) {
+        this.luck = luck;
+    }
+
+    @Override
+    public String showInfoCharacter() {
+        return super.showInfoCharacter() +
+                "Sigilo: " + stealth
+                + "Discreción: " + discretion
+                + "Suerte: " + luck;
+    }
 }
