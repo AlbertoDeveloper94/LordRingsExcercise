f02c396fdc4530ef68cd584a2966c8b46d0a7fb8
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-02-26 11:45:57 +0100

    feat: I add a MaiarValar class with the attributes, constructor and getters and setters. Also, I use a method to print the information and apply herence

--- src/main/java/org/example/alignment/neutral/MaiarValar.java
+++ src/main/java/org/example/alignment/neutral/MaiarValar.java
@@ -1,4 +1,27 @@
 package org.example.alignment.neutral;
 
-public class MaiarValar {
+import org.example.character.Character;
+
+public class MaiarValar extends Character {
+
+    private int powerCreation;
+
+    public MaiarValar(String name, String weapon, String job, int age, int life, float speed, float size, int powerCreation) {
+        super(name, weapon, job, age, life, speed, size);
+        this.powerCreation = powerCreation;
+    }
+
+    public int getPowerCreation() {
+        return powerCreation;
+    }
+
+    public void setPowerCreation(int powerCreation) {
+        this.powerCreation = powerCreation;
+    }
+
+    @Override
+    public String showInfoCharacter() {
+        return super.showInfoCharacter() +
+                "Creación de poder: " + powerCreation
+    }
 }
